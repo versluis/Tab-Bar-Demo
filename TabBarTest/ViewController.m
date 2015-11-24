fe414250b178725e0ab9e9ff17c5e982b03298c5
Jay Versluis <jay@versluis.com>
2015-11-24 18:19:22 -0500

    adding and removing view controllers to the Tab Bar works a treat

--- TabBarTest/ViewController.m
+++ TabBarTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AppDelegate.h"
 
 @interface ViewController ()
 
@@ -22,6 +23,48 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)addButtonPressed:(id)sender {
+    
+    // add another tab
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    UIViewController *redController = [storyboard instantiateViewControllerWithIdentifier:@"Red"];
+    UIViewController *greenController = [storyboard instantiateViewControllerWithIdentifier:@"Green"];
+    UIViewController *blueController = [storyboard instantiateViewControllerWithIdentifier:@"Blue"];
+    
+    redController.tabBarItem.title = @"Red";
+    greenController.tabBarItem.title = @"Green";
+    blueController.tabBarItem.title = @"Blue";
+    
+    // grab a reference to the current Tab Bar Controller
+    AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
+    UITabBarController *rootTab = (UITabBarController *)appDelegate.window.rootViewController;
+    
+    NSArray *controllers = @[self, redController, greenController, blueController];
+    [rootTab setViewControllers:controllers animated:YES];
+    // rootTab.selectedIndex = 1;
+    
+    NSLog(@"Root Tab has %lu view controllers now.", rootTab.viewControllers.count);
+    
+}
+
+
+- (IBAction)removeButtonPressed:(id)sender {
+    
+    // remove the last tab if there's more than one
+    AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
+    UITabBarController *rootTab = (UITabBarController *)appDelegate.window.rootViewController;
+    
+    if (rootTab.viewControllers.count > 1) {
+        NSMutableArray *controllers = [rootTab.viewControllers mutableCopy];
+        [controllers removeLastObject];
+        NSArray *newControllers = [NSArray arrayWithArray:controllers];
+        
+        [rootTab setViewControllers:newControllers animated:YES];
+        
+        NSLog(@"Root Tab has %lu view controllers now.", rootTab.viewControllers.count);
+
+    }
 }
 
 @end
